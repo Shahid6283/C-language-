6539a43c4252221641db0fc4480833326d4baa6b
Shahid6283 <shahid628397@gmail.com>
2021-05-28 15:36:38 +0530

    How to find the Sum of Two Numbers in C language

---
+++ Sum_of_two_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// To find the sum of two numbers 
+
+int main()
+{   
+    int a,b,c;
+    
+    printf("\n Enter the value of a =");
+    scanf("%d", &a);
+
+    printf("\n Enter the value of b =");
+    scanf("%d", &b);
+
+    c=a+b;
+    printf("\n The result is = %d",c);
+   
+    return 0;
+}
