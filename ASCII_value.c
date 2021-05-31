127694c67461ca40d72ce422a4dfcc691000cb81
Shahid6283 <shahid628397@gmail.com>
2021-05-31 15:16:41 +0530

    Find the ASCII value of a Character

---
+++ ASCII_value.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// Find the ASCII Value of a Character
+
+int main()
+{
+    char c;
+
+    printf("Enter a Character : ");
+    scanf("%c",&c);
+
+    printf("The ASCII Value of Character '%c' is : %d",c,c);
+
+    return 0;
+}
