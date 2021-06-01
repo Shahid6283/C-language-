a45e09b26b2272d49d1b3187788e347a0c1825db
Shahid <78277801+Shahid6283@users.noreply.github.com>
2021-06-01 18:11:16 +0530

    Convert the Foot Into Inches

---
+++ convert_foot_into_inches.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// Convert Foot into Inches
+
+int main()
+{
+    int f,i;
+
+    printf("Enter the foot : ");
+    scanf("%d",&f);
+
+    i=f*12;
+
+    printf("\nInches is : %d",i);
+
+    return 0;
+}
