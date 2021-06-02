3bdc824afe6731ed6737fa0b4bc512e09c6b6b8e
Shahid <78277801+Shahid6283@users.noreply.github.com>
2021-06-02 14:58:51 +0530

    Find the Largest Numbers Among the Three Numbers Using Conditional Operator

---
+++ find_largest_among_three_numbers.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+// Find the Largest Number Among the Three Numbers using if(conditional Operator)
+
+int main()
+{
+    int a,b,c;
+
+    printf("Enter the First Number : ");
+    scanf("%d",&a);
+
+    printf("Enter the Second Number : ");
+    scanf("%d",&b);
+
+    printf("Enter the Third Number : ");
+    scanf("%d",&c);
+
+    if(a>=b && a>=c){
+        printf("\nThe Greatest Number is : %d",a);
+    }
+
+    else if(b>=a && b>=c){
+        printf("\nThe Greatest Number is : %d",b);
+    }
+
+    else if(c>=a && c>=b){
+        printf("\nThe Greatest Number is : %d",c);
+    }
+
+    return 0;
+}
