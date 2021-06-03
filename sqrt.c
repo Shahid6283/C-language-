639bc7d84008507bd9bc2c40dfa56856888ace21
Shahid <78277801+Shahid6283@users.noreply.github.com>
2021-06-03 14:58:18 +0530

    Find the Square root of any Number

---
+++ sqrt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+// Find the Square root of the given number 
+
+int main()
+{
+    float a,c;
+
+    printf("Enter the Value : ");
+    scanf("%f",&a);
+
+    c = sqrt(a);
+
+    printf("\nThe Value of Square root is : %f",c);
+
+    return 0;
+}
