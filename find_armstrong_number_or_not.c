bef4ba26c3459ec5ef63cd02e1f6a549a3d2b21f
Shahid <78277801+Shahid6283@users.noreply.github.com>
2021-06-26 14:05:36 +0530

    Find that a given number is Armstrong number or not (using loops , for only 3 digits numbers)

--- find_armstrong_number_or_not.c
+++ find_armstrong_number_or_not.c
@@ -1,42 +1,35 @@
 #include<stdio.h>
 #include<math.h>
 
-// Find that a Entered Number is Armstrong Number or Not 
+// Find that a Entered Number is Armstrong Number or Not (using while loop)
 // What is Armstrong Number ---->
 // Using a simple number 153 ,we see there are 3 digits in it. If we do a simple mathematical operation of raising each of its digits to the power of 3, and then totalling the sum obtained, we get 153.
 // e.g ---> 153 = 1^3 + 5^3 + 3^3 = 153
 
 int main()
 {
-    int num,a,c,e,n;
-    float b,d,f,sum=0;
+    int num,a,n;
+    float b,sum=0;
 
     printf("Enter the Number : ");
     scanf("%d",&num);
 
     n = num;
 
-    a = n%10;
-    b = pow(a,3);
-    n = n/10;
-
-    c = n%10;
-    d = pow(c,3);
-    n = n/10;
-
-    e = n%10;
-    f = pow(e,3);
-    n = n/10;
-    
-    sum = b+d+f;
+    while(n!=0){
+        a = n%10;
+        b = pow(a,3);
+        sum+=b;
+        n = n/10;
+    }
 
     printf("\n%.2f",sum);
 
     if(num==sum){
-        printf("\nEntered Number is a Armstrong Number");
+        printf("\n%d is a Armstrong Number",num);
     }
     else{
-        printf("\nEntered Number is Not a Armstrong Number");
+        printf("\n%d is Not a Armstrong Number",num);
     }
 
     return 0;
