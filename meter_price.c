b433fb65f8274584595457754d418802e22e640b
Shahid <78277801+Shahid6283@users.noreply.github.com>
2021-07-05 17:15:01 +0530

    Add files via upload

---
+++ meter_price.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// To Calculate the Amount of meter by giving meter rate and the unit of the meter
+
+int main()
+{
+    float rate,unit,price;
+    
+    printf("Enter the Price Rate of Your Meter : ");
+    scanf("%f",&rate);
+
+    printf("Enter the Unit of Your Meter : ");
+    scanf("%f",&unit);
+
+    price = rate*unit;
+
+    printf("The Total Payable Amount is : %f",price);
+
+    return 0;
+}
