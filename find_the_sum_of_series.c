b6ac4176f11c456995514d08f84289dadb50606d
Shahid <78277801+Shahid6283@users.noreply.github.com>
2021-07-05 16:57:41 +0530

    Find the Sum of Series (1 - 8 + 27 - 64 + _ _ _ _ + n)

--- find_the_sum_of_series.c
+++ find_the_sum_of_series.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
+#include<math.h>
 
-// Find the Sum of Series of (1 + 1/2 + 1/3 + ......... + 1/n)
+// Find the Sum of Series (1 - 8 + 27 - 64 + ........ + n)
 
 int main()
 {
-    int num,i;
-    float sum=0;
+    int num,i,c,sum=0;
 
     printf("Enter the Number : ");
     scanf("%d",&num);
 
-    for(i=1; i<=num; i++){
-        sum+=1.0/i;
+    for(i=0; i<=num; i++){
+        c=i*i*i;
+        sum+=pow(-1,(i+1))*c;
     }
 
-    printf("\nThe Sum is : %f",sum);
+    printf("\nThe Sum is : %d",sum);
 
     return 0;
 }
